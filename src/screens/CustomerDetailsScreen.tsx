6bbdd31fb85c260b48222a074e69dccad31707f6
shubhamjindal30 <shubhamjindal1234@gmail.com>
2022-02-26 19:31:19 +1100

    style:  redesign customer details screen

--- src/screens/CustomerDetailsScreen.tsx
+++ src/screens/CustomerDetailsScreen.tsx
@@ -1,10 +1,26 @@
 import { useEffect } from 'react';
 import { StyleSheet, Button as DefaultButton } from 'react-native';
 import { useSelector } from 'react-redux';
+import { Card, Title } from 'react-native-paper';
 
 import { RootState } from '../store';
-import { ScrollView, Text } from '../components';
+import { ScrollView, Text, View } from '../components';
 import { RootStackScreenProps } from '../types';
+import { Theme } from '../constants';
+
+interface FieldProps {
+  title: string;
+  value: string;
+}
+
+const TitleWithStyles: React.FunctionComponent = ({ children }) => <Title style={styles.text}>{children}</Title>;
+
+const Field: React.FunctionComponent<FieldProps> = ({ title, value }) => (
+  <View style={styles.fieldView}>
+    <TitleWithStyles>{title}:</TitleWithStyles>
+    <TitleWithStyles>{value}</TitleWithStyles>
+  </View>
+);
 
 const CustomerDetailsScreen = ({ navigation, route }: RootStackScreenProps<'CustomerDetailsScreen'>) => {
   const customerId = route?.params?.customerId || null;
@@ -28,12 +44,14 @@ const CustomerDetailsScreen = ({ navigation, route }: RootStackScreenProps<'Cust
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {customer ? (
-        <>
-          <Text>First Name: {customer.firstName}</Text>
-          <Text>Last Name: {customer.lastName}</Text>
-          <Text>Region: {region ? region.name : ''}</Text>
-          <Text>Status: {customer.isActive ? 'Active' : 'Inactive'}</Text>
-        </>
+        <Card style={styles.customerCard}>
+          <Card.Content>
+            <Field title="First Name" value={customer.firstName} />
+            <Field title="Last Name" value={customer.lastName} />
+            <Field title="Region" value={region ? region.name : ''} />
+            <Field title="Status" value={customer.isActive ? 'Active' : 'Inactive'} />
+          </Card.Content>
+        </Card>
       ) : (
         <Text>Customer not found!</Text>
       )}
@@ -48,5 +66,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingBottom: 60,
     paddingTop: 30
+  },
+  customerCard: {
+    width: '80%',
+    justifyContent: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 15,
+    backgroundColor: Theme.colors.secondary
+  },
+  fieldView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'transparent'
+  },
+  text: {
+    color: Theme.colors.white
   }
 });
